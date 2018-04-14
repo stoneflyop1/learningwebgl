08aacd0f462ac2f10f344b2e96c772f4c18aa87e
jeff <jefflyop1@gmail.com>
2018-04-14 22:06:19 +0800

    chapter02

---
+++ webglutil.js
@@ -0,0 +1,49 @@
+
+/**
+ * @summary load webgl shader with type
+ * 
+ * @param {WebGLRenderingContext} gl 
+ * @param {number} type webgl shader type
+ * @param {string} source webgl shader source
+ * @returns {WebGLShader} compiled webgl shader
+ */
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {                
+        alert('An error occurred compiling the shaders: ' + 
+            gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+    return shader;
+}
+
+/**
+ * @summary initialize vertext and fragment shaders
+ * 
+ * @param {WebGLRenderingContext} gl 
+ * @param {string} vs vertext shader string
+ * @param {string} fs fragment shader string
+ * @returns {WebGLProgram} webgl context program
+ */
+function initShaders(gl, vs, fs) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vs);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fs);
+
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + 
+            gl.getProgramInfoLog(shaderProgram));
+return null;
+    }
+
+    gl.useProgram(program);
+
+    return program;
+}
